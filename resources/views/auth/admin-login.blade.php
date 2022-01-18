0f2dac7d0df8ab610dcb137a6f662f0d7b49b8aa
Ardian Danny <65061019+shatternox@users.noreply.github.com>
2022-01-18 14:04:05 +0700

    More updates

--- resources/views/auth/admin-login.blade.php
+++ resources/views/auth/admin-login.blade.php
@@ -27,8 +27,8 @@
 				<div class="row justify-content-center no-gutters">
 					<div class="col-lg-4 col-md-5 col-12">
 						<div class="content-top-agile p-10">
-							<h2 class="text-white">Get started with Us</h2>
-							<p class="text-white-50">Sign in to start your session</p>							
+							<h2 class="text-white">Funkostore Admin Panel Login</h2>
+							<p class="text-white-50">Sign in to manage product and transactions</p>							
 						</div>
 						<div class="p-30 rounded30 box-shadowed b-2 b-dashed">
                         <form method="POST" action="{{ isset($guard) ? url($guard.'/login') : route('login') }}">
@@ -50,18 +50,8 @@
                                 </div>
                             </div>
                                 <div class="row">
-                                <div class="col-6">
-                                    <div class="checkbox text-white">
-                                    <input type="checkbox" id="basic_checkbox_1" >
-                                    <label for="basic_checkbox_1">Remember Me</label>
-                                    </div>
-                                </div>
                                 <!-- /.col -->
-                                <div class="col-6">
-                                    <div class="fog-pwd text-right">
-                                    <a href="{{ route('password.request') }}" class="text-white hover-info"><i class="ion ion-locked"></i> Forgot password?</a><br>
-                                    </div>
-                                </div>
+                             
                                 <!-- /.col -->
                                 <div class="col-12 text-center">
                                     <button type="submit" class="btn btn-info btn-rounded mt-10">SIGN IN</button>
@@ -70,19 +60,6 @@
                                 </div>
                         </form>														
 
-							<div class="text-center text-white">
-							  <p class="mt-20">- Sign With -</p>
-							  <p class="gap-items-2 mb-20">
-								  <a class="btn btn-social-icon btn-round btn-outline btn-white" href="#"><i class="fa fa-facebook"></i></a>
-								  <a class="btn btn-social-icon btn-round btn-outline btn-white" href="#"><i class="fa fa-twitter"></i></a>
-								  <a class="btn btn-social-icon btn-round btn-outline btn-white" href="#"><i class="fa fa-google-plus"></i></a>
-								  <a class="btn btn-social-icon btn-round btn-outline btn-white" href="#"><i class="fa fa-instagram"></i></a>
-								</p>	
-							</div>
-							
-							<div class="text-center">
-								<p class="mt-15 mb-0 text-white">Don't have an account? <a href="auth_register.html" class="text-info ml-5">Sign Up</a></p>
-							</div>
 						</div>
 					</div>
 				</div>
